413ac163f591891a208b740729e6a484f8592eea
Albert Chen <xinguangc@twitter.com>
2017-03-02 23:57:32 -0800

    wip

--- utils/BitUtils.cs
+++ utils/BitUtils.cs
@@ -59,5 +59,33 @@ namespace Bench
         {
             return BitConverter.ToUInt64(bb, pos);
         }
+
+        public static int CeilingNextPowerOfTwo(this int x)
+        {
+            var result = 2;
+
+            while (result < x)
+            {
+                result <<= 1;
+            }
+
+            return result;
+        }
+
+        public static int Log2(int i)
+        {
+            var r = 0;
+            while ((i >>= 1) != 0)
+            {
+                ++r;
+            }
+            return r;
+        }
+
+        public static bool IsPowerOf2(this int x)
+        {
+            return x > 0 && (x & (x - 1)) == 0;
+        }
+
     }
 }
